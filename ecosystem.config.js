f7d79605158f8acebbcf77a16355f4036188a8d1
getwibix <getwibix@gmail.com>
2026-07-07 14:33:01 +0100

    fix: increase scheduler memory limit 150M->300M to prevent SIGINT loop

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,7 +25,7 @@ module.exports = {
       instances: 1,
       exec_mode: "fork",
       autorestart: true,
-      max_memory_restart: "150M",
+      max_memory_restart: "300M",
       env: { NODE_ENV: "production" },
     },
   ],
